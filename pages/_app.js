a5273313bb68f7e35aad5db679224db707526559
Juan Vazquez <juanje1019@gmail.com>
2025-07-18 10:38:55 -0400

    icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,16 @@
 import '../styles/globals.css';
 import PropTypes from 'prop-types';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 App.propTypes = {
